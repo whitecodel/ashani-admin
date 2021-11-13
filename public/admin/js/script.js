c84863b97d1f420602b115556d1d643337c5b471
White Codel <henryroy40@gmail.com>
2021-11-13 21:17:19 +0530

    snackbar added, dropdown for multi added

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -6,45 +6,60 @@ $(document).ready(function () {
     $(".nav-link").toggleClass("nav-link-active");
     $(".main").toggleClass("main-active");
     $(".dropdown").toggleClass("dropdown-active");
-    $('.submenu_icon').toggle();
+    $(".submenu_icon").toggle();
   });
   $(".smm").click(function () {
     $(".sideMenu").toggleClass("smm-active");
     $(".main").toggleClass("main-active");
   });
-  $("#home").click(function () {
-    $("#home_expand").text() == 'expand_less' ? $("#home_expand").text('expand_more') : $("#home_expand").text('expand_less');
-    $('#home_submenu').slideToggle();
-  });
+  // $("#home").click(function () {
+  //   $("#home_expand").text() == "expand_less"
+  //     ? $("#home_expand").text("expand_more")
+  //     : $("#home_expand").text("expand_less");
+  //   $("#home_submenu").slideToggle();
+  // });
+
   $(".sideMenu-li").each(function (index) {
     $(this).hover(
       function () {
-        $(this).find('.submenu_circle').css("background-color", "transparent");;
+        $(this).find(".submenu_circle").css("background-color", "transparent");
       },
       function () {
-        $(this).find('.submenu_circle').css("background-color", "#F80");;
+        $(this).find(".submenu_circle").css("background-color", "#F80");
       }
     );
   });
 });
 
 function activeSideBar(name) {
-  $("#" + name).addClass('sideMenu-li-active');
+  $("#" + name).addClass("sideMenu-li-active");
 }
 
 function logout() {
-  if (confirm('Do you really want to logout')) {
+  if (confirm("Do you really want to logout")) {
     // DO POST
     $.ajax({
       type: "POST",
       contentType: "application/json",
       url: "/admin/logout",
-    }).done(res => {
-      if (res == 'success') {
-        window.location.href = '/admin/login';
+    }).done((res) => {
+      if (res == "success") {
+        successSnakbar("Logged out successfully");
+        setTimeout(() => (window.location.href = "/admin/login"), 1000);
       } else {
-        alert(res);
+        failedSnakbar(res);
       }
     });
   }
+}
+
+function dropdown(div) {
+  div = div.parent();
+  var home = div.find("#home_expand");
+  var content = div.find("#dropdown-content");
+  home.text() == "expand_less"
+    ? home.text("expand_more")
+    : home.text("expand_less");
+
+  content.slideToggle();
 }
