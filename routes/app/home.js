7bd0efe40e167217fb2531ec6b24f7b696fb7c65
White Codel <henryroy40@gmail.com>
2021-11-14 13:41:03 +0530

    home controller added

--- routes/app/home.js
+++ routes/app/home.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
+const HomeController = require("../../controllers/app/homeController");
 
-router.get("/home", NotLoggedIn);
+router.get("/home", HomeController.home);
 
 module.exports = router;
