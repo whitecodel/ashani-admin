65b0e13480aceca4721e64c807a1c89ac90ded7b
White Codel <henryroy40@gmail.com>
2021-11-14 13:45:15 +0530

    middleware added and change password view controller also added

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -1,10 +1,11 @@
 const router = require("express").Router();
+const { NotLoggedIn } = require("../../middlewares/Adminauth");
 const AuthController = require("../../controllers/admin/authController");
 
 router.get("/login", AuthController.loginView);
 router.post("/login", AuthController.login);
-router.get("/changepassword");
-router.post("/changepassword", AuthController.changePassword);
+router.get("/changepassword", NotLoggedIn, AuthController.changePasswordView);
+router.post("/changepassword", NotLoggedIn, AuthController.changePassword);
 router.post("/logout", AuthController.logout);
 
 module.exports = router;
